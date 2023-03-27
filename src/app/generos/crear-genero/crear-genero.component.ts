6d9225c5fbadc98a49252e0cf67c2f25b20290cb
Lester René Santos Martínez <lester.santos@datys.cu>
2023-03-27 14:56:23 -0400

    Add movie components

---
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { generoCreacionDTO } from '../genero';
+
+@Component({
+  selector: 'app-crear-genero',
+  templateUrl: './crear-genero.component.html',
+  styleUrls: ['./crear-genero.component.css'],
+})
+export class CrearGeneroComponent {
+  constructor(private router: Router) {}
+
+  guardarCambios(genero: generoCreacionDTO) {
+    // guardar cambios
+    console.log(genero);
+    this.router.navigate(['/generos']);
+  }
+}
